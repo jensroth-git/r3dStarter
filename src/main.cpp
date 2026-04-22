bf85ea4dbd8ef156b94326f9c6eb63aaa829b670
Jens Roth <98889125+jensroth-git@users.noreply.github.com>
2026-04-22 02:27:14 +0200

    Update CMake configuration and main application logic. Set raylib git tag to 5.5, enable assimp installation, and improve instance handling in main.cpp for better performance and clarity.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
+#include <r3d.h>
 #include <raylib.h>
 #include <raymath.h>
-#include <r3d.h>
 #include <vector>
 
 static R3D_Mesh meshSphere = {0};
@@ -14,16 +14,16 @@ int main()
 	const auto window_height = 720;
 
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    SetTargetFPS(60);
 
 	InitWindow(window_width, window_height, "Hello Raylib!");
+	SetTargetFPS(60);
 
 	if (!IsWindowReady()) {
 		TraceLog(LOG_ERROR, "Window failed to initialize");
 		return 1;
 	}
 
-	R3D_Init(window_width, window_height, 0);
+	R3D_Init(window_width, window_height);
 
 	camDefault = (Camera3D) {
 		.position = (Vector3) {0, 2, 2},
@@ -32,44 +32,61 @@ int main()
 		.fovy = 60,
 	};
 
-    R3D_Light light = R3D_CreateLight(R3D_LIGHT_DIR);
-    {
-        R3D_SetLightDirection(light, (Vector3) { 0, -1, 0 });
-        R3D_SetLightActive(light, true);
-    }
-
-	std::vector<Matrix> instances;
+	R3D_Light light = R3D_CreateLight(R3D_LIGHT_DIR);
+	{
+		R3D_SetLightDirection(light, (Vector3) {0, -1, 0});
+		R3D_SetLightActive(light, true);
+	}
 
 	const int xInstances = 7;
 	const int yInstances = 7;
 
+	const int instanceCount = xInstances * yInstances;
+
 	const float spacing = 1.0f;
+
+	R3D_InstanceBuffer instances = R3D_LoadInstanceBuffer(
+		xInstances * yInstances, R3D_INSTANCE_POSITION | R3D_INSTANCE_ROTATION | R3D_INSTANCE_SCALE | R3D_INSTANCE_COLOR
+	);
+
+	Vector3 *positions = (Vector3 *)R3D_MapInstances(instances, R3D_INSTANCE_POSITION);
+	Quaternion *rotations = (Quaternion *)R3D_MapInstances(instances, R3D_INSTANCE_ROTATION);
+	Vector3 *scales = (Vector3 *)R3D_MapInstances(instances, R3D_INSTANCE_SCALE);
+	Color *colors = (Color *)R3D_MapInstances(instances, R3D_INSTANCE_COLOR);
 
 	// center the instances
 	const float offsetX = (xInstances - 1) * spacing / 2;
 	const float offsetZ = (yInstances - 1) * spacing / 2;
 
+
 	for (int x = 0; x < xInstances; x++) {
 		for (int y = 0; y < yInstances; y++) {
-			instances.push_back(MatrixTranslate(x * spacing - offsetX, 0, y * spacing - offsetZ));
+
+			positions[y * xInstances + x] = (Vector3) {x * spacing - offsetX, 0, y * spacing - offsetZ};
+			rotations[y * xInstances + x] = QuaternionIdentity();
+			scales[y * xInstances + x] = (Vector3) {1, 1, 1};
+			colors[y * xInstances + x] = (Color) {255, 255, 255, 255};
 		}
 	}
 
-    meshSphere = R3D_GenMeshSphere(0.25f, 64, 64, true);
+	R3D_UnmapInstances(
+		instances, R3D_INSTANCE_POSITION | R3D_INSTANCE_ROTATION | R3D_INSTANCE_SCALE | R3D_INSTANCE_COLOR
+	);
+
+	meshSphere = R3D_GenMeshSphere(0.25f, 64, 64);
 	matDefault = R3D_GetDefaultMaterial();
-    {
-        matDefault.albedo.color = (Color){255, 255, 255, 255};
-    }
+	{
+		matDefault.albedo.color = (Color) {255, 255, 255, 255};
+	}
 
 	while (!WindowShouldClose()) {
-		
+
 		UpdateCamera(&camDefault, CAMERA_ORBITAL);
 
 		BeginDrawing();
 
 		R3D_Begin(camDefault);
-		R3D_SetBackgroundColor(Color {0, 0, 255, 255});
-		R3D_DrawMeshInstanced(&meshSphere, &matDefault, instances.data(), instances.size());
+		R3D_DrawMeshInstanced(meshSphere, matDefault, instances, instanceCount);
 		R3D_End();
 
 		const auto text = "Hello, Raylib and R3D!";
@@ -79,10 +96,12 @@ int main()
 		DrawText(text, window_width / 2 - text_width / 2, window_height / 2 - text_height / 2, text_height, RED);
 		EndDrawing();
 	}
-	
-	R3D_UnloadMesh(&meshSphere);
 
+	R3D_UnloadInstanceBuffer(instances);
+	R3D_UnloadMaterial(matDefault);
+	R3D_UnloadMesh(meshSphere);
 	R3D_Close();
+
 	CloseWindow();
 	return 0;
 }
